d9b0129c4a68aa4f340ffab1819126e11b5e9dba
Gabriel <tetradog@gmail.com>
2019-01-24 22:30:03 +0100

    ahora es más fiable :D

--- String.py
+++ String.py
@@ -1,28 +1,43 @@
 import re
 import Tupla
-
+import InternetSource.charRead as charRead
 def Format(string,*args):
-	i=0
-	string=str(string)
-	args=Tupla.Limpia(args)
-	f=len(args)
-	while i<f:
-		ptrn='\{'+str(i)+'\}'
-		patron=re.compile(ptrn)
-		strAPoner=str(args[i])
-		string=patron.sub(strAPoner,string)
+	i = 0
+	string = str(string)
+	args = Tupla.Limpia(args)
+	f = len(args)
+	while i < f:
+		ptrn = '\{' + str(i) + '\}'
+		patron = re.compile(ptrn)
+		strAPoner = str(args[i])
+		string = patron.sub(strAPoner,string)
 		i+=1
 	return string
 
 def Split(string,*strsSplit):
-	strSplit=''
-	strsSplit=Tupla.Limpia(strsSplit)
-	if len(strsSplit)==0:
-		strsSplit=(';')#mejor pongo un caracter por defecto
+	strSplit = ''
+	strsSplit = Tupla.Limpia(strsSplit)
+	if len(strsSplit) == 0:
+		strsSplit = (';')#mejor pongo un caracter por defecto
 		#raise Exception()#se necesitan caracteres para el split
 	for s in strsSplit:
-		strSplit+=s+'|'
+		strSplit+=str(s) + '|'
 	
-	return re.split(strSplit[:-1],string)
+	return re.split(strSplit[:-1],str(string))
+
+def Enter():
+  enter=None
+  try:
+       charRead._GetchWindows()
+       enter="\n\r"
+  except ImportError:
+       enter="\n"
+  return enter
+
+def Replace(string,strOld,strNew):
+    patron=re.compile(str(strOld))
+    return patron.sub(str(strNew),str(string))
+
+
 
 	
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import Console
 import String
 
-
+Console.WriteLine("El intro en este sistema es {0}",String.Replace(String.Replace(String.Enter(),'\n','/n'),'\r','/r'))
 Console.WriteLine("Ahora leeré un solo caracter!")
 chr=Console.ReadKey()
 Console.WriteLine("El caracter es '{0}'",chr)

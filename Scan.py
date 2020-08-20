7fd205ee05faebc57d479011d99b9983a49fe352
Gabriel Cortés Garcia <tetradog@gmail.com>
2020-08-20 10:51:27 +0200

    Update Scan.py

--- Scan.py
+++ Scan.py
@@ -11,15 +11,17 @@ class ScannedPort:
 		self.IsOpen=isOpen
 
 # Print a nice banner with information on which host we are about to scan
+def IsOpen(ip,port):
+	sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+	result = sock.connect_ex((ip, port))
+	isOk= result == 0
+	if isOk:
+		sock.close()
+	return isOk
 def Scan(ip,initPort,endPort):
 	try:
 		for port in range(initPort,endPort):
-			sock = socket.socket(socket.AF_INET, 			   socket.SOCK_STREAM)
-			result = sock.connect_ex((ip, port))
-			isOk=result == 0
-			if isOk:
-				sock.close()	
-			yield ScannedPort(port,isOk)
+			yield ScannedPort(port,IsOpen(ip,port))
 	except KeyboardInterrupt:
 		Console.WriteLine("You pressed Ctrl+C")
 	except socket.gaierror:
